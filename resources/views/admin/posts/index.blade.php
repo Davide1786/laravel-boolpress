2247569ff88c5b7fac1beba3ae9d456f3e0364d8
Davide <dadavidenapolitano@gmail.com>
2021-11-05 14:39:34 +0100

    Fine prima parte

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    @dump($posts)
+    <ul>
+        @foreach ($posts as $post)
+            <li><a href="{{ route('admin.posts.show', $post->id)}}">{{ $post->title }}</a></li>
+            {{-- <div class="card">
+                <div class="card-header">
+                  {{$post->title}}
+                </div>
+                <div class="card-body">
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="{{ route('admin.posts.show') }}" class="btn btn-primary">Vai al dettaglio</a>
+                </div>
+              </div> --}}
+        @endforeach
+    </ul>
 @endsection
